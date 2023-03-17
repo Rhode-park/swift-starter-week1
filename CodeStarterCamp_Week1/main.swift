d782cb2d4363e345636f3194b84065e1a27616c3
Rhode-park <jinah.park.16@gmail.com>
2023-03-17 21:02:44 +0900

    feat: PaepaeroFactory 구조체 내부 drawPaepaero, drawPaepaeroInformation, drawPaepaeroBody, drawPaepaeroBar 메서드 구현

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -6,19 +6,51 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-func drawIcecreamBody() {
-    let lines = 8
-    for _ in 1...lines {
-        print("***********")
+import Foundation
+
+struct PaepaeroFactory {
+    func drawPaepaero(bodyLength: Int, body: String, topping: String, barLength: Int) {
+        drawPaepaeroInformation(bodyLength: bodyLength, body: body, topping: topping, barLength: barLength)
+        drawPaepaeroBody(bodyLength: bodyLength, body: body, topping: topping)
+        drawPaepaeroBar(barLength: barLength)
+    }
+    
+    func drawPaepaeroInformation(bodyLength: Int, body: String, topping: String, barLength: Int) {
+        print(String(format: constant.information, bodyLength, body, topping, barLength))
+    }
+    
+    func drawPaepaeroBody(bodyLength: Int, body: String, topping: String) {
+        for _ in 1...bodyLength {
+            print(topping, body, topping, separator: "")
+        }
+    }
+    
+    func drawPaepaeroBar(barLength: Int) {
+        for _ in 1...barLength {
+            print(" | |")
+        }
     }
 }
 
-func drawIcecreamBar() {
-    let lines = 4
-    for _ in 1...lines {
-        print("    | |")
+extension PaepaeroFactory {
+    enum constant {
+        static let information = """
+                                <정보>
+                                길이: %d
+                                몸통: %@
+                                토핑: %@
+                                막대길이: %d
+                                
+                                """
     }
 }
 
-drawIcecreamBody()
-drawIcecreamBar()
+let paepaeroFactory = PaepaeroFactory()
+
+paepaeroFactory.drawPaepaero(bodyLength: 10, body: "***", topping: " ", barLength: 4)
+print("")
+paepaeroFactory.drawPaepaero(bodyLength: 12, body: "***", topping: "&", barLength: 4)
+print("")
+paepaeroFactory.drawPaepaero(bodyLength: 10, body: "***", topping: "#", barLength: 6)
+print("")
+paepaeroFactory.drawPaepaero(bodyLength: 6, body: "|0|", topping: " ", barLength: 4)
